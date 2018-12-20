8a2e63948c394f796a3517c6e9ae0db06413cbbf
yang1006 <731883870@qq.com>
2018-12-20 14:46:37 +0800

    理解并重新写一个OpenGL 绘制三角形

--- TestApp/app/src/main/java/yll/self/testapp/opengl/HelloTriangleActivity.java
+++ TestApp/app/src/main/java/yll/self/testapp/opengl/HelloTriangleActivity.java
@@ -3,8 +3,10 @@ package yll.self.testapp.opengl;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
+import android.opengl.GLES20;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -18,6 +20,9 @@ import javax.microedition.khronos.opengles.GL10;
 import yll.self.testapp.BaseActivity;
 import yll.self.testapp.utils.UtilsManager;
 
+/**
+ * https://blog.piasy.com/2016/06/07/Open-gl-es-android-2-part-1/
+ * */
 public class HelloTriangleActivity extends BaseActivity {
 
     private final int CONTEXT_CLIENT_VERSION = 3;
@@ -30,8 +35,9 @@ public class HelloTriangleActivity extends BaseActivity {
         mGLSurfaceView = new GLSurfaceView(this);
         if (detectOpenGLES30()){
             mGLSurfaceView.setEGLContextClientVersion(CONTEXT_CLIENT_VERSION);
+            mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
             mGLSurfaceView.setRenderer(new HelloTriangleRenderer());
-//            mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+            mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
             setContentView(mGLSurfaceView);
         }else {
             UtilsManager.log(TAG, "不支持OpenGL ES 3.0");
@@ -60,118 +66,96 @@ public class HelloTriangleActivity extends BaseActivity {
 
     private class HelloTriangleRenderer implements GLSurfaceView.Renderer {
 
-
-        private int mProgramObject;
-        private int mWidth;
-        private int mHeight;
-        private FloatBuffer mVertices;
-        private final String TAG = "HelloTriangleRenderer";
-        private final float[] mVerticesData =
-                {       0.0f, 0.5f, 0.0f,
-                        -0.5f, -0.5f, 0.0f,
-                        0.5f, -0.5f, 0.0f };
+        /**三角形的三个顶点*/
+//        private final float VERTICES[] = {
+//                -0.5f, -0.5f, 0f,
+//                0.5f, -0.5f, 0f,
+//                0f, 0.5f, 0f
+//        };
+        private final float VERTICES[] = {
+                0f, 1f, 0f,
+                -0.5f, -1f, 0f,
+                1f, -1f, 0f
+        };
+        private int mProgram;
+        //变量 gl_Position 表示顶点的位置，每个顶点都会执行一遍顶点着色器程序
+        private final String VERTEX_SHADER =
+//                         "#version 300 es \n" +
+                         "attribute vec4 vPosition; \n " +
+                         "uniform mat4 uMVPMatrix; \n" +
+                         "void main() { \n" +
+                         " gl_Position = uMVPMatrix * vPosition; \n" +
+                         "}";
+        //变量 gl_FragColor 表示fragment的颜色，每个fragment都会执行一遍片段着色器程序
+        private final String FRAGMENT_SHADER =
+//                        "#version 300 es \n" +
+                        "precision mediump float; \n" +
+                        "void main() { \n" +
+                        "    gl_FragColor = vec4(0.5, 0.5, 0, 1); \n" +
+                        " }";
+        private FloatBuffer mVertexBuffer;
+        private int mMvpMatrixHandle;
+        private float[] mMVPMatrix = new float[16];
 
         HelloTriangleRenderer(){
-            mVertices = ByteBuffer.allocateDirect(mVerticesData.length * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
-            mVertices.put(mVerticesData).position(0);
+            mVertexBuffer = ByteBuffer.allocateDirect(VERTICES.length * 4)
+                    .order(ByteOrder.LITTLE_ENDIAN)
+                    .asFloatBuffer()
+                    .put(VERTICES);
+            mVertexBuffer.position(0);
         }
 
-        //创建着色器对象，加载和编译着色器
-        private int loadShader(int type, String shaderStr){
-            int shader;
-            int[] compiled = new int[1];
-            //创建 shader对象
-            shader = GLES30.glCreateShader(type);
-            if (shader == 0){
-                return 0;
-            }
-            //加载 shader source
-            GLES30.glShaderSource(shader, shaderStr);
-            // compile the shader
+        private int loadShader(int type, String shaderSource){
+            int shader = GLES30.glCreateShader(type);
+            GLES30.glShaderSource(shader, shaderSource);
             GLES30.glCompileShader(shader);
-            // check the compile status
-            GLES30.glGetShaderiv(shader, GLES30.GL_COMPILE_STATUS, compiled, 0);
-            if (compiled[0] == 0){
-                UtilsManager.log(TAG, GLES30.glGetShaderInfoLog(shader));
-                GLES30.glDeleteShader(shader);
-            }
             return shader;
         }
 
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            // 初始化着色器和程序对象
-            String vShaderStr =
-                    "#version 300 es 			  \n"                 //声明着色器版本 v3.00
-                            +   "in vec4 vPosition;           \n"
-                            + "void main()                  \n"
-                            + "{                            \n"
-                            + "   gl_Position = vPosition;  \n"
-                            + "}                            \n";
 
-            String fShaderStr =
-                    "#version 300 es		 			          	\n"
-                            + "precision mediump float;					  	\n"
-                            + "out vec4 fragColor;	 			 		  	\n"
-                            + "void main()                                  \n"
-                            + "{                                            \n"
-                            + "  fragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );	\n"
-                            + "}                                       \n";
+            GLES30.glClearColor(0f, 0f, 0f, 0f);
+            mProgram = GLES30.glCreateProgram();
+            int vertexShader = loadShader(GLES30.GL_VERTEX_SHADER, VERTEX_SHADER);
+            int fragmentShader = loadShader(GLES30.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+            GLES30.glAttachShader(mProgram, vertexShader);
+            GLES30.glAttachShader(mProgram, fragmentShader);
+            GLES30.glLinkProgram(mProgram);
+            GLES30.glUseProgram(mProgram);
+            /**获取shader代码中的变量索引*/
+            int mPositionHandle = GLES30.glGetAttribLocation(mProgram, "vPosition");
+            mMvpMatrixHandle = GLES30.glGetUniformLocation(mProgram, "uMVPMatrix");
+            /**启用 顶点 */
+            GLES30.glEnableVertexAttribArray(mPositionHandle);
+            /**绑定 顶点 坐标值
+             * param: 要配置的顶点，顶点属性大小， 数据类型，是否需要被标准化，步长，顶点数据
+             * https://learnopengl-cn.github.io/01%20Getting%20started/04%20Hello%20Triangle/#_7
+             * */
+            GLES30.glVertexAttribPointer(mPositionHandle, 3, GLES30.GL_FLOAT, false, 12, mVertexBuffer);
 
-            int vertexShader;
-            int fragmentShader;
-            int programObject;
-            int[] linked = new int[1];
-            // 加载顶点和片段着色器
-            vertexShader = loadShader(GLES30.GL_VERTEX_SHADER, vShaderStr);
-            fragmentShader = loadShader(GLES30.GL_FRAGMENT_SHADER, fShaderStr);
-            // 创建程序对象
-            programObject = GLES30.glCreateProgram();
-            if (programObject == 0){
-                return;
-            }
-            //将顶点和片段着色器连接到程序对象上
-            GLES30.glAttachShader(programObject, vertexShader);
-            GLES30.glAttachShader(programObject, fragmentShader);
-            //bind vPosition to attribute 0 顶点位置加载到GL，并连接到顶点着色器声明的vPosition属性
-            GLES30.glBindAttribLocation(programObject, 0, "vPosition");
-            //link the program
-            GLES30.glLinkProgram(programObject);
-            //check the link status
-            GLES30.glGetProgramiv(programObject, GLES30.GL_LINK_STATUS, linked, 0);
-            if (linked[0] == 0){
-                UtilsManager.log(TAG, "error linking program:");
-                UtilsManager.log(TAG, GLES30.glGetProgramInfoLog(programObject));
-                GLES30.glDeleteProgram(programObject);
-                return;
-            }
 
-            mProgramObject = programObject;
-            //清除颜色，应该在glClear之前调用
-            GLES30.glClearColor(1f, 1f, 1f, 0f);
         }
 
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height) {
-            mWidth = width;
-            mHeight = height;
+            GLES30.glViewport(0, 0, width, height);
+            //openGL坐标系和 Android 手机坐标系不是线性对应的，需要做投影变化(projection)
+            Matrix.perspectiveM(mMVPMatrix, 0, 45, width * 1f / height, 0.1f, 100f);
+            Matrix.translateM(mMVPMatrix, 0, 0f, 0f, -2.5f);
+
         }
 
         @Override
         public void onDrawFrame(GL10 gl) {
-            // Set the viewport（视口）  通知用于绘制的2D渲染表面的原点和宽高
-            GLES30.glViewport(0, 0, mWidth, mHeight);
-            // Clear the color buffer 清除颜色缓冲区
             GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
 
-            // Use the program object
-            GLES30.glUseProgram(mProgramObject);
+            //在绘制的时候为 uMVPMatrix 赋值
+            GLES30.glUniformMatrix4fv(mMvpMatrixHandle, 1, false, mMVPMatrix, 0);
 
-            // Load the vertex data   指定顶点数组
-            GLES30.glVertexAttribPointer(0, 3, GLES30.GL_FLOAT, false, 0, mVertices);
-            GLES30.glEnableVertexAttribArray(0);
-            //通知绘制图元
             GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 0, 3);
         }
     }
+
+
 }
